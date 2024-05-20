5d9ac44723a829eebc431872c32abb26ec1cfc60
hilinxinhui <hilinxinhui@gmail.com>
2024-05-20 20:26:25 +0800

    commit 2024 01 15 report

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-xelatex NEU_Beamer_Slide
-xelatex NEU_Beamer_Slide
+xelatex NEU_Beamer_Slide.tex
+xelatex NEU_Beamer_Slide.tex
 rm -f *.aux *.bak *.log *.bbl *.dvi *.blg *.thm *.toc *.out *.lof *.lol *.lot *.nav *.snm *.fdb_latexmk *.synctex.gz
